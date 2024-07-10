440ad53ecb9c6f39509f212bc171a06b050c7b6a
Bishwajit Adhikary <55208330+bishwajitcadhikary@users.noreply.github.com>
2024-07-10 15:52:10 +0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -45,8 +45,6 @@ then
 
 	ssh -i /root/.ssh/id_rsa -t $SSH_USER@$SSH_HOST "sudo chown -R $OWNER:$OWNER $PATH_SOURCE"
 	ssh -i /root/.ssh/id_rsa -t $SSH_USER@$SSH_HOST "sudo chmod 775 -R $PATH_SOURCE"
-	ssh -i /root/.ssh/id_rsa -t $SSH_USER@$SSH_HOST "sudo chmod 777 -R $PATH_SOURCE/storage"
-	ssh -i /root/.ssh/id_rsa -t $SSH_USER@$SSH_HOST "sudo chmod 777 -R $PATH_SOURCE/public"
 
 	echo $'\n' "------ CONGRATS! DEPLOY SUCCESSFUL!!! ---------" $'\n'
 	exit 0
